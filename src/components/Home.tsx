a47c19bbbbc9a44ff57da70053b7b346fae7df71
Thavish Perera <thavish.perera@gmail.com>
2021-07-08 18:04:30 -0400

    Populate and wire up Items component

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,19 +1,44 @@
-import {
-  Box,
-  Center,
-  Container,
-  HStack,
-  Text,
-  VStack,
-  Button,
-} from 'native-base';
-import React from 'react';
-import {SafeAreaView, TouchableOpacity} from 'react-native';
+import React, {useContext, useState} from 'react';
+import {Box, Center, Text, Button, Modal, useToast} from 'native-base';
 import NumPad from './NumPad';
+import AppContext from '../AppContext';
+
 const Home = () => {
   const context = useContext(AppContext);
   const charges = context.charges;
   const setCharges = context.setCharges;
+
+  const [text, setText] = useState('0.00');
+
+  const toast = useToast();
+
+  const buttonPressed = (inputChar: string): void => {
+    if (inputChar === 'C') {
+      setText('0.00');
+    } else {
+      if (text == '0.00') {
+        setText(inputChar);
+      } else {
+        setText(text.concat(inputChar));
+      }
+    }
+  };
+
+  const submitItem = () => {
+    // Take raw item value
+    if (/^\d+(.\d{1,2})?$/.test(text) && text != '0.00') {
+      setCharges(charges.concat([{name: '_item', price: Number(text)}]));
+    } else if (text != '0.00') {
+      toast.show({
+        status: 'error',
+        description: 'Please enter a valid number',
+        placement: 'top',
+      });
+    }
+    setText('0.00');
+  };
+
+  const total = charges.reduce((acc, item) => acc + item.price, 0).toFixed(2);
 
   return (
     <Box
@@ -21,7 +46,8 @@ const Home = () => {
       height="100%"
       alignItems="center"
       justifyContent="flex-end"
-      safeArea>
+      pt={5}
+      safeAreaBottom>
       <Box flex={1} width="100%" px={10} justifyContent="flex-start">
         <Button size="md" onPress={() => navigation.navigate('Items')}>
           Items List
@@ -33,8 +59,11 @@ const Home = () => {
             borderColor="primary.500"
             width="100%"
             height={10}>
-            <Text color="primary.500">Charge: $0.00</Text>
+            <Text color="primary.500">Charge: ${total}</Text>
           </Center>
+          <Text alignSelf="flex-end" pt={5}>
+            {text} CAD
+          </Text>
         </Box>
       </Box>
       <NumPad buttonPressed={buttonPressed} submit={submitItem} />
--- src/components/Items.tsx
+++ src/components/Items.tsx
@@ -1,10 +1,40 @@
-import {Box, Text} from 'native-base';
-import React from 'react';
+import {AddIcon, Box, HStack, List, Text} from 'native-base';
+import React, {useContext} from 'react';
+import {TouchableOpacity} from 'react-native';
+import AppContext from '../AppContext';
 
 const Items = () => {
+  const context = useContext(AppContext);
+  const charges = context.charges;
+  const setCharges = context.setCharges;
+
+  const addItem = (item: {name: string; price: number}) => {
+    setCharges(charges.concat([item]));
+  };
+
+  const listItems = [
+    {name: 'Banana', price: 1},
+    {name: 'Apple', price: 2},
+    {name: 'Mango', price: 3},
+  ].map((item, index) => (
+    <List.Item key={index}>
+      <HStack justifyContent="space-between" flex={1} pr={5}>
+        <Text color="primary.500">{item.name}</Text>
+        <TouchableOpacity onPress={() => addItem(item)}>
+          <AddIcon color="primary.500" />
+        </TouchableOpacity>
+      </HStack>
+    </List.Item>
+  ));
+
   return (
-    <Box safeArea>
-      <Text>Items</Text>
+    <Box
+      width="100%"
+      height="100%"
+      // alignItems="center"
+      justifyContent="flex-start"
+      safeAreaBottom>
+      <List space={2}>{listItems}</List>
     </Box>
   );
 };
