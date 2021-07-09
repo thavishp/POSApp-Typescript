31193822ed7e68131c92428d2a39ba06cc4b5243
Thavish Perera <thavish.perera@gmail.com>
2021-07-09 18:02:29 -0400

    Add ability to change item quantities in cart

--- src/components/CartModal.tsx
+++ src/components/CartModal.tsx
@@ -1,35 +1,87 @@
-import React, {useContext} from 'react';
+import React, {useContext, useEffect} from 'react';
 import {Box, Button, HStack, Modal, Text, VStack} from 'native-base';
 import AppContext from '../AppContext';
+import {TouchableOpacity} from 'react-native';
+import {Charge} from '../types';
 
 const CartModal = ({showModal, setShowModal}: any) => {
   const context = useContext(AppContext);
   const charges = context.charges;
   const setCharges = context.setCharges;
+  const [, forceUpdate] = React.useState({});
+
+  const decrementItem = (index: number) => {
+    if (charges[index].quantity == 1) {
+      setCharges((prev: Array<Charge>) => {
+        prev.splice(index, 1);
+        return prev;
+      });
+    } else {
+      // charges[index].quantity -= 1;
+      setCharges((prev: Array<Charge>) => {
+        prev[index].quantity -= 1;
+        return prev;
+      });
+      console.log('decrementItem');
+    }
+    forceUpdate({});
+  };
+
+  const incrementItem = (index: number) => {
+    setCharges((prev: Array<Charge>) => {
+      prev[index].quantity += 1;
+      return prev;
+    });
+    console.log('incrementItem');
+    forceUpdate({});
+  };
+
+  const clearCart = () => {
+    setCharges([]);
+  };
 
   const cartItems = charges.map((item, index) => (
     <Box key={index}>
       <HStack space={3} justifyContent="space-between">
-        <Text>{item.name == '_item' ? 'Item ' + (index + 1) : item.name}</Text>
-        <Text>
-          ${item.price.toFixed(2)} x {item.quantity}
-        </Text>
+        <VStack>
+          <Text fontSize={20}>
+            {item.name == '_item' ? 'Item ' + (index + 1) : item.name}
+          </Text>
+          <Text fontSize={15}>${item.price.toFixed(2)}</Text>
+        </VStack>
+        <HStack space={3} alignItems="center">
+          <TouchableOpacity onPress={() => decrementItem(index)}>
+            <Text fontSize={25}>-</Text>
+          </TouchableOpacity>
+          <Text fontSize={20}>{item.quantity}</Text>
+          <TouchableOpacity onPress={() => incrementItem(index)}>
+            <Text fontSize={25}>+</Text>
+          </TouchableOpacity>
+        </HStack>
       </HStack>
     </Box>
   ));
 
   return (
     <Modal isOpen={showModal} onClose={() => setShowModal(false)}>
-      <Modal.Content maxWidth="400px">
+      <Modal.Content maxWidth="400px" pt={5} px={0}>
         <Modal.CloseButton />
-        <Modal.Header>Cart</Modal.Header>
-        <Modal.Body>
+        <Modal.Header pl={5}>Cart</Modal.Header>
+        <Modal.Body px={5}>
           <VStack space={3} pt={5}>
-            {cartItems}
+            {cartItems.length != 0 ? (
+              cartItems
+            ) : (
+              <Text alignSelf="center">Cart Empty</Text>
+            )}
           </VStack>
         </Modal.Body>
-        <Modal.Footer>
-          <Button.Group variant="ghost" space={2}>
+        <Modal.Footer width="100%">
+          <Button.Group
+            variant="ghost"
+            justifyContent="space-around"
+            width="100%">
+            <Button onPress={clearCart}>CLEAR CART</Button>
             <Button>CHECK OUT</Button>
             <Button
               onPress={() => {
